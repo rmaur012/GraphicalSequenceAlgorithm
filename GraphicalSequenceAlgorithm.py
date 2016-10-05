905c2bc83e55e3aae8c576aaec93076aa4ae17d4
Ricardo Maury <rmaur012@fiu.edu>
2016-10-05 01:35:38 -0400

    Update Script
    Updated the code to also determine whether or not the sum of the degrees in the sequence is divisible by 2.
    
    If it is divisible by 2, then the sequence may be graphical.
    If it is not divisible by 2, then the sequence is not graphical.

--- GraphicalSequenceAlgorithm.py
+++ GraphicalSequenceAlgorithm.py
@@ -4,17 +4,25 @@ sizeOfSeq = int(sizeOfSeq)
 index = 0
 sequence = [None]*sizeOfSeq
 
-#The user inputs the degrees of the vertices, then sorts it in reverse
+#The user inputs the degrees of the vertices, then sorts it in reverse.
+#It will also calculate sum of the sequence of degrees to then be evaluated in the second if statement after the loop
+seqSum = 0
 while sizeOfSeq>0:
         sequence[index]=int(raw_input("Enter The Degree For A Vertex: "))
+        seqSum = seqSum + sequence[index]
         sizeOfSeq=sizeOfSeq-1
         index=index+1
 sequence.sort(reverse = True)
 
-#Checks if a degree is bigger or equal to number of vertices, proceeds with algorithm if it's not
+#Checks if a degree is bigger or equal to number of vertices, 
+#then will check if the sum of the sequence = 2*(number of edges), which would make it not graphical if it does not equal,
+#then it proceeds with algorithm if it's not determined to be either of the first two
 if sequence[0]>=len(sequence):
         print 'The Sequence Is Not Graphical: Degree Is Higher Than The Number Of Vertices'
 
+elif seqSum%2 !=0:
+        print 'The Sequence Is Not Graphical: The Sum Of The Degrees In The Sequence Is Not Divisible By 2'
+        
 else:   #Algorithm starts here
         seqIndex = 1
         iteration = 0
